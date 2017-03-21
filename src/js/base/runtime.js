4b6fe67fe3f49384d32f2041f546349d3499c931
lucbories <luc.bories@gmail.com>
2017-03-21 22:17:32 +0100

    Split of devapt main package

---
+++ src/js/base/runtime.js
@@ -0,0 +1,12 @@
+
+// NPM IMPORTS
+
+// COMMON IMPORTS
+
+let _runtime_instance = undefined
+
+export const register_runtime = (arg_runtime)=> {
+	_runtime_instance = arg_runtime
+}
+
+export default _runtime_instance
